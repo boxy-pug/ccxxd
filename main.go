f48d03e55c2aa20170508ece15c7b3dd93db0c93
boxy-pug <wewepipi99@pm.me>
2025-04-22 14:36:31 +0200

    finally got that spacing fixed...

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 
 type Config struct {
 	file               *os.File
-	fileBytes          int64
-	littleEndianOutput bool //-e
-	byteGrouping       int  // -g <int> default 2
-	cols               int  // -c <int> octets per line. default 16
-	len                int  // -l <int> stop writing after len octets
-	seek               int  // -s <offset> (which byte to start reading from)
-	revert             bool // -r Reverse operation: convert (or patch) hex dump into binary
+	endByte            int64
+	littleEndianOutput bool  //-e
+	byteGrouping       int   // -g <int> default 2
+	cols               int   // -c <int> octets per line. default 16
+	len                int64 // -l <int> stop writing after len octets
+	seek               int64 // -s <offset> (which byte to start reading from)
+	revert             bool  // -r Reverse operation: convert (or patch) hex dump into binary
 }
 
 func main() {
@@ -34,8 +34,8 @@ func loadConfig() Config {
 	flag.BoolVar(&cfg.revert, "r", false, "Reverse operation: convert (or patch) hex dump into binary.")
 	flag.IntVar(&cfg.byteGrouping, "g", 2, "Separate the output of every <bytes> bytes (two hex characters or eight bit digits each) by a whitespace.")
 	flag.IntVar(&cfg.cols, "c", 16, "Format <cols> octets per line. Default 16")
-	flag.IntVar(&cfg.len, "l", -1, "Stop after writing <len> octets.")
-	flag.IntVar(&cfg.seek, "s", -1, "Start at <seek> bytes abs. (or rel.) infile offset.")
+	flag.Int64Var(&cfg.len, "l", -1, "Stop after writing <len> octets.")
+	flag.Int64Var(&cfg.seek, "s", 0, "Start at <seek> bytes abs. (or rel.) infile offset.")
 
 	flag.Parse()
 
@@ -52,7 +52,12 @@ func loadConfig() Config {
 			fmt.Printf("couldn't retrieve file stat for %v: %v", cfg.file, err)
 			os.Exit(1)
 		}
-		cfg.fileBytes = info.Size()
+		if cfg.len >= 0 {
+			cfg.endByte = cfg.len
+		} else {
+			cfg.endByte = info.Size()
+		}
+
 	} else if len(args) == 0 {
 		cfg.file = os.Stdin
 	}
@@ -61,35 +66,53 @@ func loadConfig() Config {
 }
 
 func printLines(cfg Config) {
-	offSet := int64(0)
+	offSetHex := cfg.seek
+	offSetChar := cfg.seek
 	reader := bufio.NewReader(cfg.file)
+	_, err := cfg.file.Seek(cfg.seek, 0)
 
-	for offSet < cfg.fileBytes {
-		buffer := make([]byte, 16)
-		bt, err := io.ReadFull(reader, buffer)
+	if err != nil {
+		fmt.Printf("error setting offset: %v", err)
+		os.Exit(1)
+	}
+
+	for offSetHex < cfg.endByte {
+		buffer := make([]byte, cfg.cols)
+		_, err := io.ReadFull(reader, buffer)
 		if err != nil {
 			fmt.Printf("error: %v", err)
 		}
 
-		fmt.Printf("%08x: ", offSet)
+		// Printing offset
+		fmt.Printf("%08x: ", offSetHex)
+
+		// Printing hex octs
 		for i, byt := range buffer {
 			fmt.Printf("%02x", byt)
 			if (i+1)%2 == 0 {
 				fmt.Printf(" ")
 			}
+			offSetHex++
+			if offSetHex == cfg.endByte {
+				printExtraSpace(len(buffer), i)
+				break
+			}
 		}
 
+		// Printing ascii
 		for _, charByt := range buffer {
 			if isValidASCII(charByt) {
 				fmt.Printf("%s", string(charByt))
 			} else {
 				fmt.Printf(".")
 			}
-
+			offSetChar++
+			if offSetChar == cfg.endByte {
+				break
+			}
 		}
 
 		fmt.Println()
-		offSet += int64(bt)
 	}
 }
 
@@ -97,20 +120,10 @@ func isValidASCII(b byte) bool {
 	return b >= 32 && b <= 126
 }
 
-/*
-
-r := strings.NewReader("some io.Reader stream to be read\n")
-
-	buf := make([]byte, 4)
-	if _, err := io.ReadFull(r, buf); err != nil {
-		log.Fatal(err)
+func printExtraSpace(bufLen, index int) {
+	missingBytes := bufLen - (index + 1)
+	remainingSpace := missingBytes*2 + (missingBytes+1)/2
+	for range remainingSpace {
+		fmt.Printf(" ")
 	}
-	fmt.Printf("%s\n", buf)
-
-	// minimal read size bigger than io.Reader stream
-	longBuf := make([]byte, 64)
-	if _, err := io.ReadFull(r, longBuf); err != nil {
-		fmt.Println("error:", err)
-	}
-
-*/
+}
